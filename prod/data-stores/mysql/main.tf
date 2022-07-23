55a8138b809bb4e85dd69280b29934df2f8396f5
jmikula <jmikula@am-jmikula-m67.home>
2022-07-23 00:29:33 -0400

    added backend files for the webserver clusters and fixed production files

--- prod/data-stores/mysql/main.tf
+++ prod/data-stores/mysql/main.tf
@@ -3,13 +3,12 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "example" {
-  identifier_prefix   = var.identifier_prefix
-  allocated_storage   = var.allocated_storage
-  engine              = var.engine
-  instance_class      = var.instance_class
+  identifier_prefix   = "data-store-prod"
+  allocated_storage   = 10
+  engine              = "mysql"
+  instance_class      = "db.t2.micro"
   db_name             = var.db_name
   password            = var.db_password
-  username            = var.username
+  username            = var.db_username
   skip_final_snapshot = true
 }
-
--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -1,17 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
 module "webserver-cluster" {
-  source = "github.com/jmik618/tf_up_and_running//services?ref=v0.0.1"
+  source = "github.com/jmik618/modules.git//services/webserver-cluster?ref=v0.0.1"
 
-  cluster_name       = "webservers-prod"
-  remote_bucket_name = "tf-up-and-running-state-jm"
-  remote_bucket_key  = "prod/services/webserver-cluster/terraform.tfstate"
-  instance_type      = "t3.large"
-  min_size           = 4
-  max_size           = 15
-  data-stores        = module.data-stores
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
+  instance_type          = "t2.micro"
+  min_size               = 4
+  max_size               = 15
 }
 
 resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
